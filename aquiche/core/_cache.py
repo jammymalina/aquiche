e0a60b32bcf4bbefbc7d40d5c39d104012bd47f9
jammymalina <jakub.jantosik@gmail.com>
2022-09-26 00:48:48 +0200

    Updated

--- aquiche/core/_cache.py
+++ aquiche/core/_cache.py
@@ -1,8 +1,8 @@
-import asyncio
+from asyncio import Lock, Event, sleep as asleep, iscoroutinefunction
 from dataclasses import dataclass
 from datetime import datetime, timedelta, timezone
 import random
-from typing import Any, Awaitable, Optional
+from typing import Any, Awaitable, Optional, Tuple
 
 
 class CacheTaskExecutionInfo:
@@ -14,54 +14,73 @@ class CacheTaskExecutionInfo:
 @dataclass
 class CachedValue:
     last_fetched: Optional[datetime] = None
-    inflight: Optional[asyncio.Condition] = None
+    inflight: Optional[Event] = None
     value: Any = None
 
 
 class CachedRecord:
-    __mutex: asyncio.Lock
+    __mutex: Lock
     __refresh_interval: Optional[timedelta]
     __cached_value: CachedValue
 
     def __init__(self, refresh_interval: Optional[timedelta] = None) -> None:
-        self.__mutex = asyncio.Lock()
+        self.__mutex = Lock()
         self.__refresh_interval = refresh_interval
         self.__cached_value = CachedValue()
 
     async def get_cached(self, task: Awaitable[Any], task_exec_info: Optional[CacheTaskExecutionInfo] = None) -> Any:
-        async with self.__mutex:
-            if self.__cached_value.last_fetched is not None and (
-                self.__refresh_interval is None
-                or (datetime.now(timezone.utc) - self.__cached_value.last_fetched) < self.__refresh_interval
-            ):
-                return self.__cached_value.value
+        event = Event()
+        await self.__mutex.acquire()
 
-            if self.__cached_value.inflight is None:
-                self.__cached_value.inflight = asyncio.Condition()
-                task_exec_info = task_exec_info or CacheTaskExecutionInfo()
-                await self.__execute_cache_task(task=task, exec_info=task_exec_info)
+        if self.__cached_value.last_fetched is not None and (
+            self.__refresh_interval is None
+            or (datetime.now(timezone.utc) - self.__cached_value.last_fetched) < self.__refresh_interval
+        ):
+            self.__mutex.release()
+            return self.__cached_value.value
 
-        await self.__cached_value.inflight.wait()
+        if self.__cached_value.inflight is not None:
+            event = self.__cached_value.inflight
+            self.__mutex.release()
+        else:
+            self.__cached_value.inflight = Event()
+            event = self.__cached_value.inflight
+            self.__mutex.release()
+
+            task_exec_info = task_exec_info or CacheTaskExecutionInfo()
+            await self.__store_cache(task=task, exec_info=task_exec_info)
+
+        await event.wait()
+
         return self.__cached_value.value
 
-    async def __execute_cache_task(self, task: Awaitable[Any], exec_info: CacheTaskExecutionInfo) -> None:
-        retry_iter = 0
+    async def __store_cache(self, task: Awaitable[Any], exec_info: CacheTaskExecutionInfo) -> None:
+        if self.__cached_value.inflight is None:
+            raise RuntimeError("Aquiche internal error - possible deadlock")
+        value, is_error = await self.__execute_task(task=task, exec_info=exec_info)
+
+        async with self.__mutex:
+            event = self.__cached_value.inflight
+            self.__cached_value.value = value
+            self.__cached_value.last_fetched = datetime.now(timezone.utc)
+            self.__cached_value.inflight = None
+            event.set()
+
+        if is_error and exec_info.fail:
+            raise value
+
+    async def __execute_task(self, task: Awaitable[Any], exec_info: CacheTaskExecutionInfo) -> Tuple[Any, bool]:
         while True:
             try:
-                self.__cached_value.value = await task
-                self.__cached_value.last_fetched = datetime.now(timezone.utc)
+                if iscoroutinefunction(task):
+                    return (await task(), False)
+                return (await task, False)
             except Exception as err:
                 if retry_iter >= exec_info.retries:
-                    if exec_info.fail:
-                        raise
-                    self.__cached_value.value = err
-                    self.__cached_value.last_fetched = datetime.now(timezone.utc)
+                    return err, True
 
                 if exec_info.backoff_in_seconds != 0:
                     sleep_seconds = exec_info.backoff_in_seconds * 2**retry_iter + random.uniform(0, 1)
-                    await asyncio.sleep(sleep_seconds)
+                    await asleep(sleep_seconds)
 
                 retry_iter += 1
-            finally:
-                if self.__cached_value.inflight is not None:
-                    self.__cached_value.inflight.notify_all()
--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,10 +1,32 @@
-from aquiche.core._cache import CachedValue
+from asyncio import sleep as asleep, gather
+import functools
+
+from pytest_mock import MockerFixture
+
+from aquiche.core._cache import CachedValue, CachedRecord, CacheTaskExecutionInfo
 
 
-def test_default_ceched_value():
+def test_default_cached_value() -> None:
     """It should create default CachedValue item"""
     cached_value = CachedValue()
 
     assert cached_value.value is None
     assert cached_value.inflight is None
     assert cached_value.last_fetched is None
+
+
+async def test_cache_stampede(mocker: MockerFixture) -> None:
+    """It should execute task only once even when multiple cache calls at the same are made"""
+    cached_record = CachedRecord()
+    task = mocker.AsyncMock(return_value=42)
+
+    values = await gather(
+        cached_record.get_cached(task=task, task_exec_info=CacheTaskExecutionInfo()),
+        cached_record.get_cached(task=task, task_exec_info=CacheTaskExecutionInfo()),
+        cached_record.get_cached(task=task, task_exec_info=CacheTaskExecutionInfo()),
+        cached_record.get_cached(task=task, task_exec_info=CacheTaskExecutionInfo()),
+    )
+
+    for value in values:
+        assert value == 42
+    task.assert_called_once()
